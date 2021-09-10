d0191b4db0ebec21246e866c9fff5e81c1606b8a
HyperMeansHigh <90439397+HyperMeansHigh@users.noreply.github.com>
2021-09-10 15:06:49 +0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,13 @@
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
 #include <stdio.h>
 #include "function.h"
 int main()
 {
-  return 0;
+  int a,b,c,d;
+  printf("Plz input a,b,c,d (int):\n");
+  scanf("%d %d %d %d",&a,&b,&c,&d);
+  swap(&c,&d);
+  printf("The sum of a and b is: %d\nAfter swapping c and d is:%d %d\n",add(a,b),c,d);
+  return EXIT_SUCCESS;
 }
